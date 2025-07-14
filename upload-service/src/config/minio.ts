08465112e18a6e3ecbb70b7663efebe1870c89ba
Saad Hasan <saadh393@gmail.com>
2025-07-14 22:18:27 +0600

    feat(minio): update configuration to use environment variables for MinIO connection

--- upload-service/src/config/minio.ts
+++ upload-service/src/config/minio.ts
@@ -3,10 +3,11 @@ import { Client } from "minio";
 export const BUCKET = "videos";
 
 export const minio = new Client({
-  endPoint: "localhost",
-  port: 9000,
-  accessKey: "ROOTUSER",
-  secretKey: "CHANGEME123",
+  endPoint: process.env.MINIO_HOST || "localhost",
+  port: Number(process.env.MINIO_PORT) || 9000,
+  accessKey: process.env.MINIO_ROOT_USER || "ROOTUSER",
+  secretKey: process.env.MINIO_ROOT_PASSWORD || "CHANGEME123",
+  useSSL: false,
 });
 
 export default minio;
